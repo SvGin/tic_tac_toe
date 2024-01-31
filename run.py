25301220e9a3feee0bee21c690ee9b466b6e092e
SvGin <>
2024-01-31 11:57:21 +0000

    Creating run.py file and begining function with rules of the game

---
+++ run.py
@@ -0,0 +1,15 @@
+def begining():
+    """
+    Introduction to the game:
+    Welcome message and game rules.
+
+    """
+    print("Welcome to TIC TAC TOE\n")
+    print("Rules of the game are:\n Choose your marking between O and X\n"
+    "Players will take turns and place their simbols on the grid\n "
+    "In order to win one of the plyers need to place 3 of the symbols in a horizontal, vertical or diogonal row\n "
+    "If this is not achieved then it is a tie and the game will restart \n")
+    input("Press Enter to continue")
+
+begining()
+
